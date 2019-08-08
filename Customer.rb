cfd383a5998ec953fcd0fe485348b18a5541c4be
Hayden Westfield-Bell <wickertongue@Users-MacBook-Pro.local>
2019-08-08 15:27:16 +0100

    working on customer borrowing a book

--- Customer.rb
+++ Customer.rb
@@ -4,6 +4,16 @@ attr_reader :name
 
   def initialize(name)
     @name = name
+    @books = []
   end
+
+  def add_book_to_customer(book)
+    @books << book
+  end
+
+  def customer_book_count
+    return @books.count
+  end
+
 
 end
--- Library.rb
+++ Library.rb
@@ -7,5 +7,17 @@ class Library
     @stock = []
   end
 
-  
+  def add_book_to_stock(book)
+    @stock << book
+  end
+
+  def stock_count
+    return @stock.count
+  end
+
+  def remove_book(book)
+    @stock.delete(book)
+  end
+
+
 end
